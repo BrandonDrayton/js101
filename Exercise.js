21128fb4b51d39d13da1d0a966694954a1c5443b
Brandon Drayton <brandondrayton@gmail.com>
2022-04-22 16:08:06 -0400

    What i got done today so far

--- Exercise.js
+++ Exercise.js
@@ -1,1 +1,137 @@
-Test
+// console.log('Hello World')
+
+// def function():
+
+// Madlib
+
+// function madlib(name, subject) {
+//     console.log(`I can't believe ${name} isn't any good at ${subject}. Eventually ${name} will be better at ${subject}`)
+// }
+
+// madlib("Brandon", "Flexbox")
+
+// Tip Calculator 
+
+
+
+// function tipAmount(billAmount, levelOfService) {
+
+//     if (levelOfService == "bad") {
+//         return (billAmount * .1)
+//     } if (levelOfService == "fair") {
+//         return (billAmount * .15)
+//     } if (levelOfService == "good") {
+//         return (billAmount * .2)
+//     }
+// }
+
+// console.log(tipAmount(444, "good"))
+
+// Tip Calculator 2
+
+// function totalAmount(billAmount, levelOfService) {
+//     return (tipAmount(billAmount, levelOfService) + billAmount)
+// }
+// console.log(totalAmount(444, "good"))
+
+// Work or Sleep In
+
+// Write a script that asks for a day of the week. If the day is a business day, print 'go to work!'. If the day is a weekend day, print 'sleep in!'. If whatever the user entered is not a day, print 'enter a valid day'.
+
+// const dayOfTheWeek = "saturday"
+
+
+// if (dayOfTheWeek == "monday") {
+//     console.log("go to work!")
+// }
+
+// if (dayOfTheWeek == "tuesday") {
+//     console.log("go to work!")
+// }
+
+// if (dayOfTheWeek == "wednesday") {
+//     console.log("go to work!")
+// }
+
+// if (dayOfTheWeek == "thursday") {
+//     console.log("go to work!")
+// }
+
+// if (dayOfTheWeek == "friday") {
+//     console.log("go to work!")
+// }
+
+// else console.log("sleep in!")
+
+
+
+// Write a script that asks the user for a password. If the user enters the correct word (pick one yourself) then print 'Welcome!', otherwise print 'Try Again!'
+
+// const password = "Bring me another"
+
+// if (password == "Bring me another") {
+//     console.log("Welcome!");
+// } else (console.log("Pick one yourself!"))
+
+// Day of Week
+// Write a script that asks the user for a number between 1-7. Print the corresponding day for the number, (i.e. 1 = 'Sunday', 2 = 'Monday' etc). If the input is invalid, print an error message.
+
+// const input = 4
+
+// if (input == 1) {
+//     console.log("Sunday");
+// }
+// if (input == 2) {
+//     console.log("Monday");
+// }
+// if (input == 3) {
+//     console.log("Tuesday");
+// }
+// if (input == 4) {
+//     console.log("Wednesday");
+// }
+// if (input == 5) {
+//     console.log("Thursday");
+// }
+// if (input == 6) {
+//     console.log("Friday");
+// }
+// if (input == 7) {
+//     console.log("Saturday")
+// }
+
+// Letter Grade
+// Write a script that asks for a score number between 0-100. Print the corresponding grade for that number. (i.e. <60 = F, <70 = D, <80 = C, <90 = B, 90+ = A). If the input is invalid, print an error message.
+
+// const score = "Test"
+
+
+// if (score < 60) {
+//     console.log("F");
+// } if (score < 70) {
+//     console.log("D");
+// } if (score < 80) {
+//     console.log("C");
+// } if (score < 90) {
+//     console.log("B");
+// } if (score >= 90) {
+//     console.log("A");
+// }
+// else (console.log("ERROR"))
+
+
+// Picnic Game
+// Write a function that returns true if the two input strings start with the same letter. Write a script that asks the user for their name. Ask them for the food they are bringing to the picnic. Use the function to check if the food matches the name. If it does, tell them they can come to the party. If it doesn't, tell them to try again.
+
+// const names = "John Smith"
+// const food = "Pizza"
+
+
+// function question(names, food) {
+//     if (names[0] == food[0])
+//      console.log("Come to the Party!");
+//     else console.log("Go away!");
+// }
+
+// console.log(question())
+
